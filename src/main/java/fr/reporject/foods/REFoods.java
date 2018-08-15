15288b1ed4add53c693af68ec176ed90f28ab49c
AlasDiablo <marcolet.pierre@laposte.net>
2018-08-15 12:56:42 +0200

    add basic java class

--- src/main/java/fr/reporject/foods/REFoods.java
+++ src/main/java/fr/reporject/foods/REFoods.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package fr.reporject.foods;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+@Mod(modid = REFoods.MODID, name = REFoods.NAME, version = REFoods.VERSION)
+public class REFoods {
+    public static final String MODID = "refoods";
+    public static final String NAME = "§dRE: Foods";
+    public static final String VERSION = "1.0.0";
 
     private static Logger logger;
 
@@ -26,6 +25,6 @@ public class ExampleMod
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("Hello World!");
     }
 }
