5096ee235ad6585981c3de569584648a5fb33cd6
AlasDiablo <marcolet.pierre@laposte.net>
2018-08-19 12:29:12 +0200

    update to 1.0.1
    add last version of recore

--- src/main/java/fr/reproject/foods/REFoods.java
+++ src/main/java/fr/reproject/foods/REFoods.java
@@ -1,5 +1,6 @@
 package fr.reproject.foods;
 
+import fr.reproject.core.REModList;
 import fr.reproject.foods.init.REFoodsTab;
 import fr.reproject.foods.proxy.CommonProxy;
 import net.minecraft.creativetab.CreativeTabs;
@@ -10,12 +11,13 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = REFoods.MODID, name = REFoods.NAME, version = REFoods.VERSION, dependencies = "required:forge@[14.23.4.2705,);required-after:recore@[1.0.2,);")
+@Mod(modid = REFoods.MODID, name = REFoods.NAME, version = REFoods.VERSION, dependencies = "required:forge@[14.23.4.2705,);required-after:recore@[1.0.3,);")
 public class REFoods {
     public static final String MODID = "refoods";
-    public static final String NAME = "§dRE: Foods";
+    public static final String NAME = "RE: Foods";
     public static final String VERSION = "1.0.1";
     public static final String CLIENT_PROXY_CLASSE = "fr.reproject.foods.proxy.ClientProxy";
+    public static int id;
 
     @SidedProxy(clientSide = REFoods.CLIENT_PROXY_CLASSE)
     public static CommonProxy proxy;
@@ -26,6 +28,8 @@ public class REFoods {
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        id = REModList.loadMod(MODID, NAME);
+        REModList.initLoadMod(id);
         logger = event.getModLog();
         proxy.preInit();
         proxy.render();
